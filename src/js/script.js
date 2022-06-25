c70924cbd822f7e6acc864deb48369ceaf1141bc
shokouhSkr <shokouhskr@gmail.com>
2022-06-25 16:30:48 +0430

    set botton click sound

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,9 @@
 "use strict";
+
+// set button click sound
+const audio = new Audio();
+audio.src = "/src/assets/click.mp3";
+
+document.querySelector(".btnSound").addEventListener("click", () => {
+  audio.play();
+});
