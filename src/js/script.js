871b1e5363a25869b54960d502ab49191e358b18
shokouhSkr <shokouhskr@gmail.com>
2022-06-26 23:26:26 +0430

    open modal and close modal functions added

--- src/js/script.js
+++ src/js/script.js
@@ -4,10 +4,39 @@
 const audio = new Audio();
 audio.src = "/src/assets/click.mp3";
 
-const btnSound = document.querySelector(".btn-sound");
+const btnsSound = document.querySelectorAll(".btn-sound");
 
-btnSound.addEventListener("click", () => {
-  audio.play();
-  console.log("clicked!");
-});
+for (let i = 0; i < btnsSound.length; i++) {
+  btnsSound[i].addEventListener("click", () => {
+    audio.play();
+    // console.log("clicked!");
+  });
+}
+
 /*********************************************************************/
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModaL = document.querySelector(".close-modal");
+const btnsOpenModal = document.querySelectorAll(".show-modal");
+
+// functions
+const openModal = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+// show modal
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener("click", openModal);
+}
+
+// hide modal by close button
+btnCloseModaL.addEventListener("click", closeModal);
+
+// hide modal by click on somewhere
+overlay.addEventListener("click", closeModal);
